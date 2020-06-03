6188686e88055565bb8a657f52761460cdb9ccca
GabrielHenrique <gabrielhenriqueg@users.noreply.github.com>
2020-06-03 20:35:19 -0300

    BeeHoney

---
+++ click-img.js
@@ -0,0 +1,11 @@
+var x = document.getElementById("image-display-handler");
+
+function openNav() {
+  x.style.width = "100%";
+  x.style.display = "flex"
+  x.style.visibility = 'visible'
+}
+
+function closeNav() {
+  x.style.width = "0%";
+}
